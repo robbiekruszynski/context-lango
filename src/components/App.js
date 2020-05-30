c6f246d43d994c10ce4a01efe3d45ed88efc652a
robbie <robbiekruszynski@gmail.com>
2020-05-30 13:25:25 -0700

    add button, field, usercreate files

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,44 @@
 import React from "react";
+import UserCreate from "./UserCreate";
 
 class App extends React.Component {
   state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
   render() {
     return (
       <div className="ui container">
         <div>
           Select a language:
-          <i className="flag us" />
-          <i className="flag it" />
-          <i className="flag pl" />
-          <i className="flag fr" />
-          <i className="flag jp" />
-          <i className="flag mx" />
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag it"
+            onClick={() => this.onLanguageChange("Italian")}
+          />
+          <i
+            className="flag pl"
+            onClick={() => this.onLanguageChange("Polish")}
+          />
+          <i
+            className="flag fr"
+            onClick={() => this.onLanguageChange("French")}
+          />
+          <i
+            className="flag jp"
+            onClick={() => this.onLanguageChange("Japanese")}
+          />
+          <i
+            className="flag mx"
+            onClick={() => this.onLanguageChange("Spanish")}
+          />
         </div>
+        {this.state.language}
+        <UserCreate />
       </div>
     );
   }
